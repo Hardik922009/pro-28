bb7b7b6ca5fefbb2375931ce0d185b4085aad95f
Hardik922009 <76473556+Hardik922009@users.noreply.github.com>
2021-10-27 19:05:12 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,39 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 5,}
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.sling);
+        }
+        attach(body){
+            this.sling.bodyA = body;
+        }
+        
+        fly(){
+            this.sling.bodyA = null;
+        }
+        display(){
+            push();
+            
+            stroke(48,22,8);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.sling3,pointA.x -30, pointA.y -10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                image(this.launcher,pointA.x + 25, pointA.y -10,15,30);
+            }
+           
+            
+            pop();
+        }
+    }
